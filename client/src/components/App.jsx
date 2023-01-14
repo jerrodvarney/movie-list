6ee35c12b0819b56bc71211f5ba6b2340deeadc0
Jerrod Varney <jerrodvarney@gmail.com>
2023-01-13 17:57:16 -0800

    Created server and database functionality. Set up client side components up to Learn level 1 requirements.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,81 @@
 import React from 'react';
+import axios from 'axios';
+import MovieList from './MovieList.jsx';
+import UserInput from './UserInput.jsx';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+const useEffect = React.useEffect;
+const useState = React.useState;
+
+
+const App = (props) => {
+
+  ///// STATE DATA PIECES /////
+  const [allMovies, setAllMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [unWatched, setUnWatched] = useState([]);
+
+  ///// CURRENT RENDERED MOVIES - PRIMARY STATE /////
+  const [currentMovies, setCurrentMovies] = useState([]);
+
+
+
+  ///// EVENT HANDLERS /////
+  const handleSearch = (event)=> {
+    const inputField = event.target.previousSibling
+    const searchTerm = inputField.value;
+    if (searchTerm.length) {
+      let matches = [];
+      for (let movie of allMovies) {
+        if (movie.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+          matches.push(movie);
+        }
+      }
+      if (!matches.length) {
+        alert(`No movies matching ${searchTerm} found. Please try again!`);
+      } else {
+        setCurrentMovies(matches);
+      }
+      inputField.value = '';
+    } else {
+      alert('Please enter a movie title to search for.');
+    }
+  };
+
+  const handleAddMovie = (event) => {
+
+  };
+
+  const handleWatchButton = (event) => {
+
+  };
+
+  const handleDisplayWatched = (event) => {
+
+  };
+
+
+
+  ///// HTTP REQUEST HANDLERS /////
+  const url = 'http://127.0.0.1:3000/api';
+
+  const initialize = () => {
+    axios.get(url + '/movies')
+      .then((res) => {
+        setCurrentMovies(res.data); // - render the page with all movies stored on the database - //
+        setAllMovies(res.data); // - store all movie data to avoid unnecessary client/server interactions - //
+      }
+    );
+  };
+
+  ///// INITIALIZES THE APP /////
+  useEffect(initialize, []);
+
+  return (
+    <div>
+      <UserInput handleSearch={handleSearch} />
+      <MovieList movies={currentMovies} />
+    </div>
+  )
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const app = express();
 const PORT = 3000 || process.env.PORT;
 
+// Local Database
+const db = require('./db');
+
+
+// Middleware
+const morgan = require('morgan');
+const cors = require('cors');
+
+
+// Router
+const router = require('./routes.js');
+
+
+// Create server
+const app = express();
+
+
+// Logging and parsing
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+
+// Request Handlers
+app.use('/api', router);
+
+
+// Serve the client files
 app.use(express.static('client/dist'));
 
+
+// Runs server on port 3000
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 })
